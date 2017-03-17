a9fa653126c1732262df9ac1ce5fc07aa4557168
Stephen Brennan <smb196@case.edu>
2017-03-16 23:13:30 -0400

    Document respond plugin

--- plugins/plugins.go
+++ plugins/plugins.go
@@ -1,6 +1,12 @@
 /*
 This library contains a set of core plugins for the slacksoc bot. To register
-these plugins, simply use the provided Register() function.
+these plugins, simply use the provided Register() function. Below is a list of
+provided plugins (none of their implementations are publicly accessible).
+
+Respond is a plugin which allows you to register triggers and one or more
+responses to those triggers. In functionality, it is pretty much identical to
+Slackbot. Its configuration is a list of "response" objects, each of which
+contains "trigger" (a string), and "replies" (a list of strings).
 */
 package plugins
 
